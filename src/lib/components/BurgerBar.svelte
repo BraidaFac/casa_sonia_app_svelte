8f1fa729d441d636ecd2aeb13ca6e85925f665f9
BraidaFac <facundobraida98@gmail.com>
2024-03-20 13:10:27 -0300

    buggg solved

--- src/lib/components/BurgerBar.svelte
+++ src/lib/components/BurgerBar.svelte
@@ -77,7 +77,7 @@
 	{/if}
 </div>
  
-{#if !loading}
+{#if loading}
 <div class="z-40 absolute w-full top-1/3 pl-6">
 <p class="text-4xl my-5  text-center animate-bounce">Cargando articulos</p><ProgressRadial
 				value={loadingValue}
